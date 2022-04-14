174b1d42b3004121ceb5725be4f11b0523ccac50
zakaria-elghiati <102788738+zakaria-elghiati@users.noreply.github.com>
2022-04-14 14:47:18 +0000

    Add files via upload

---
+++ SportOrg/src/view/MaFenetre.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author zakariae
+ */
+public class MaFenetre {
+    JLabel titre1 = new javax.swing.JLabel();
+
+    
+    
+}
